e4f36bd252ab72ec8dd32db77c514cd9c41c14fc
Aniket Kale <vu4s2223001@pvppcoe.ac.in>
2023-10-23 22:14:47 +0530

    Trial Commit

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -73,7 +73,7 @@ const Footer = () => {
                         <div className="foot_copyright">
                             <p>
                                 {currYear} | XBeat. All Rights Reserved.
-                                Built by | <a href="https://gulshansongara.netlify.app/">Gulshan Songara</a>
+                                Built by | <a href="https://github.com/TheImperialOne/">Aniket D. Kale</a>
                             </p>
                         </div>
                         <div className="foot_social">
