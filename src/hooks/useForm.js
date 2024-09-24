245639fa87e8d45b5ee29de6a74775a12c74f74c
Pappya <Pappyajadhav73@gmail.com>
2024-09-24 19:12:04 +0530

    Did backend Authentication

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,98 +1,96 @@
-import { useContext, useState } from "react";
+import {useContext, useState} from "react";
 import commonContext from "../contexts/common/commonContext";
 
 const useForm = () => {
-  const { toggleForm, setFormUserInfo } = useContext(commonContext);
-  const [inputValues, setInputValues] = useState({});
-  // const [isLoggedIn, setisLoggedIn] = useState(false);
+    const {toggleForm, setFormUserInfo} = useContext(commonContext);
+    const [inputValues, setInputValues] = useState({});
+    // const [isLoggedIn, setisLoggedIn] = useState(false);
 
-  // handling input-values
-  const handleInputValues = (e) => {
-    const { name, value } = e.target;
+    // handling input-values
+    const handleInputValues = (e) => {
+        const {name, value} = e.target;
 
-    setInputValues((prevValues) => {
-      return {
-        ...prevValues,
-        [name]: value,
-      };
-    });
-  };
-
-  const handleLogin = (loggedUser) => {
-    try {
-      fetch("http://localhost:5000/Login-user", {
-        method: "POST",
-        crossDomain: true,
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-          "Access-Control-Allow-Orgin": "*",
-        },
-        body: JSON.stringify({
-          mail: inputValues.mail,
-          password: inputValues.password,
-        }),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          if (data.status === "Login sucess") {
-            window.alert(`Hello ${loggedUser}, you're successfully logged-in.`);
-            setInputValues({});
-            setFormUserInfo(loggedUser);
-            toggleForm(false);
-            // setisLoggedIn(true);
-          } else {
-            window.alert("Invail Credentails !!");
-            setInputValues({});
-          }
+        setInputValues((prevValues) => {
+            return {
+                ...prevValues, [name]: value,
+            };
         });
-    } catch (e) {}
-  };
+    };
 
-  const handleSignUp = () => {
-    try {
-      fetch("http://localhost:5000/auth", {
-        method: "POST",
-        crossDomain: true,
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-          "Access-Control-Allow-Orgin": "*",
-        },
-        body: JSON.stringify({
-          username: inputValues.username,
-          mail: inputValues.mail,
-          password: inputValues.password,
-        }),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          if (data.status === "OK") {
-            window.alert(`you're successfully Registered.`);
-            window.alert(`Please Login to continue`);
-            setInputValues({});
-          }
-          else{
-            window.alert(data.status);
-            setInputValues({});
-          }
-        });
-    } catch (e) {}
-  };
+    const handleLogin = (loggedUser) => {
+        try {
+            fetch("http://localhost:5000/Login-user", {
+                method: "POST", crossDomain: true, headers: {
+                    "Content-Type": "application/json", Accept: "application/json", "Access-Control-Allow-Orgin": "*",
+                }, body: JSON.stringify({
+                    mail: inputValues.mail, password: inputValues.password,
+                }),
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    if (data.status === "Login sucess") {
+                        window.alert(`Hello ${loggedUser}, you're successfully logged-in.`);
+                        setInputValues({});
+                        setFormUserInfo(loggedUser);
+                        toggleForm(false);
+                        // setisLoggedIn(true);
+                    } else {
+                        window.alert("Invail Credentails !!");
+                        setInputValues({});
+                    }
+                });
+        } catch (e) {
+        }
+    };
 
-  // handling form-submission
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    const loggedUserInfo = inputValues.mail.split("@")[0].toUpperCase();
+    const handleSignUp = () => {
+        try {
+            fetch("http://localhost:5000/auth", {
+                method: "POST", crossDomain: true, headers: {
+                    "Content-Type": "application/json", Accept: "application/json", "Access-Control-Allow-Orgin": "*",
+                }, body: JSON.stringify({
+                    username: inputValues.username, mail: inputValues.mail, password: inputValues.password,
+                }),
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    if (data.status === "OK") {
+                        window.alert(`you're successfully Registered.`);
+                        window.alert(`Please Login to continue`);
+                        setInputValues({});
+                    } else {
+                        window.alert(data.status);
+                        setInputValues({});
+                    }
+                });
+        } catch (e) {
+        }
+    };
 
-    if (inputValues.username === undefined) {
-      handleLogin(loggedUserInfo);
-    } else {
-      handleSignUp();
-    }
-  };
+    // handling form-submission
+    const handleFormSubmit = async (e) => {
+        e.preventDefault();
+        const endpoint = isSignupVisible ? '/api/auth/register' : '/api/auth/login';
 
-  return { inputValues, handleInputValues, handleFormSubmit };
+        try {
+            const response = await fetch(endpoint, {
+                method: 'POST', headers: {
+                    'Content-Type': 'application/json',
+                }, body: JSON.stringify(inputValues),
+            });
+
+            const data = await response.json();
+            if (response.ok) {
+            } else {
+                alert(data.message);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+
+    return {inputValues, handleInputValues, handleFormSubmit};
 };
 
 export default useForm;
